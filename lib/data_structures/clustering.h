d2fa0ee5fd1030b0067334514d47069731e86bb0
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-03-19 16:44:01 -0500

    graph and clustering ds

---
+++ lib/data_structures/clustering.h
@@ -0,0 +1,73 @@
+#ifndef CLUSTERING_H
+#define CLUSTERING_H
+
+#include <unordered_map>
+#include <vector>
+#include <stdexcept>
+
+// namespace reccs {
+
+/**
+ * @brief Class that efficiently stores mappings from node IDs to cluster IDs.
+ */
+class Clustering {
+private:
+    std::unordered_map<int, int> node_to_cluster;  // Maps node_id to cluster_id
+
+public:
+    /**
+     * @brief Default constructor.
+     */
+    Clustering() = default;
+
+    /**
+     * @brief Adds a node to a specific cluster.
+     * 
+     * @param node_id The ID of the node to add.
+     * @param cluster_id The ID of the cluster to add the node to.
+     */
+    void add_node_to_cluster(int node_id, int cluster_id) {
+        node_to_cluster[node_id] = cluster_id;
+    }
+
+    /**
+     * @brief Get the cluster ID for a given node.
+     * 
+     * @param node_id The ID of the node to query.
+     * @return The cluster ID of the node.
+     * @throws std::out_of_range if the node is not assigned to any cluster.
+     */
+    int get_cluster(int node_id) const {
+        return node_to_cluster.at(node_id);
+    }
+
+    /**
+     * @brief Check if a node has been assigned to a cluster.
+     * 
+     * @param node_id The ID of the node to check.
+     * @return true if the node is assigned to a cluster, false otherwise.
+     */
+    bool has_node(int node_id) const {
+        return node_to_cluster.find(node_id) != node_to_cluster.end();
+    }
+
+    /**
+     * @brief Get the number of nodes assigned to clusters.
+     * 
+     * @return The number of nodes with cluster assignments.
+     */
+    size_t size() const {
+        return node_to_cluster.size();
+    }
+
+    /**
+     * @brief Clear all node-to-cluster mappings.
+     */
+    void clear() {
+        node_to_cluster.clear();
+    }
+};
+
+// } // namespace reccs
+
+#endif // CLUSTERING_H
--- lib/data_structures/graph.h
+++ lib/data_structures/graph.h
@@ -1,2 +1,71 @@
 #pragma once
 
+#include <vector>
+#include <unordered_map>
+#include <stdexcept>
+
+template <typename T>
+class Graph {
+private:
+    struct Edge {
+        int to;
+        
+        Edge(int to) : to(to) {}
+    };
+    
+    std::unordered_map<T, int> node_to_index;
+    std::vector<T> nodes;
+    std::vector<std::vector<Edge>> adjacency_list;
+
+public:
+    Graph() = default;
+    
+    // Add a node to the graph
+    void add_node(const T& node) {
+        if (node_to_index.find(node) == node_to_index.end()) {
+            node_to_index[node] = nodes.size();
+            nodes.push_back(node);
+            adjacency_list.push_back(std::vector<Edge>());
+        }
+    }
+    
+    // Add an edge between two nodes
+    void add_edge(const T& from, const T& to) {
+        // Add nodes if they don't exist
+        add_node(from);
+        add_node(to);
+        
+        int from_idx = node_to_index[from];
+        int to_idx = node_to_index[to];
+        
+        // Add edge
+        adjacency_list[from_idx].push_back(Edge(to_idx));
+    }
+    
+    // Check if a node exists
+    bool has_node(const T& node) const {
+        return node_to_index.find(node) != node_to_index.end();
+    }
+    
+    // Get all nodes
+    const std::vector<T>& get_nodes() const {
+        return nodes;
+    }
+    
+    // Get all neighbors of a node
+    std::vector<T> get_neighbors(const T& node) const {
+        if (!has_node(node)) {
+            throw std::runtime_error("Node not found in graph");
+        }
+        
+        int node_idx = node_to_index.at(node);
+        std::vector<T> neighbors;
+        
+        for (const Edge& edge : adjacency_list[node_idx]) {
+            neighbors.push_back(nodes[edge.to]);
+        }
+        
+        return neighbors;
+    }
+
+};
