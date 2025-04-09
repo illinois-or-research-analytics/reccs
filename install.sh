56010a4acc43fdf7ad01ea14b79892490362d3f3
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-04-09 17:39:17 -0500

    python requirements

--- install.sh
+++ install.sh
@@ -24,5 +24,9 @@ fi
 
 cd ../../../
 
+# Install python dependencies
+conda activate gt
+pip install -r requirements.txt
+
 export CMAKE_PREFIX_PATH=$HOME/local:$CMAKE_PREFIX_PATH
 export igraph_DIR=$HOME/local/lib/cmake/igraph
