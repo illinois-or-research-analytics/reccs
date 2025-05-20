c7fd4c8e3b435fb13ae08d37f2f1ea6f5ecd85c5
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-05-20 01:38:19 -0500

    reccs experiment

---
+++ experiments/reccs_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#####################################################
+##        Important parameters of your job         ##
+##             are specified here 		   ##
+#####################################################
+
+#SBATCH --time=4:00:00				## total computing time
+#SBATCH --nodes=1				## number of nodes 
+#SBATCH --ntasks-per-node=1			## number of tasks per node
+#SBATCH --cpus-per-task=64			## number of CPUs per task
+#SBATCH --mem=128GB				## memory per node
+#SBATCH --partition=folkvangr			## queue
+#SBATCH --output=reccs_test_cit_full2.out		## file that will receive output from execution
+#SBATCH --error=reccs_test_cit_full2.err		## file that will receive any error messages
+#SBATCH --job-name=csr_runtime_test		## job name
+#SBATCH --mail-user=vikramr2@illinois.edu
+#SBATCH --mail-type=BEGIN,END
+
+########## Run your executable ######################
+
+cd ../build
+cmake .. && make
+time ./reccs ../data/cen.tsv -v -c ../data/cen_0.01.tsv 
