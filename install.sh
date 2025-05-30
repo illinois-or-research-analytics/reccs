b73cc938d9e5f1fc95d87c23b4993ce498fee9f5
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-05-30 01:18:01 -0500

    better setup script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# Check if gt environment exists, if not create it
+if ! conda env list | grep "gt$"; then
+    conda create --name gt -c conda-forge graph-tool
+fi
+
+
 # Install python dependencies
 conda activate gt
 pip install -r requirements.txt
